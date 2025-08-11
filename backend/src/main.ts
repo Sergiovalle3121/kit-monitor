b2ef9b6648dbd59a19e4fab16000faeb38959b11
Sergiovalle3121 <sergiovallezarate@gmail.com>
2025-08-11 01:47:35 -0600

    Fix: usar process.env.PORT para Railway

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -22,6 +22,6 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
 
   const port = process.env.PORT || 3000;
-  await app.listen(port, '0.0.0.0');
+ await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
