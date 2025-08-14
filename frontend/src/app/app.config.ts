ebe0fe2ecd5e9ee7294769648a556107cddd2a1c
Sergiovalle3121 <sergiovallezarate@gmail.com>
2025-08-14 02:19:45 -0600

    fix(frontend): provide HttpClient + Animations y router-outlet

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,17 @@
+// frontend/src/app/app.config.ts
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { jwtInterceptor } from './core/jwt-interceptor';
+
+import { provideAnimations } from '@angular/platform-browser/animations'; // <- necesario para Angular Material
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([jwtInterceptor])),
+    provideHttpClient(withInterceptors([jwtInterceptor])), // <- HttpClient para tus servicios
+    provideAnimations(),                                    // <- Animations para Material
   ],
 };
